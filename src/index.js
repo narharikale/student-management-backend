000db42d7662c3147b7c3cddcaafdfde1097e56b
narharikale <n7kale@gmail.com>
2025-01-15 20:09:33 +0530

    feat: add index.html and serve it on root route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import studentRouter from "./routes/studentroutes.js";
 import errorHandler from "./middleware/errorhandler.js";
+import path from "path";
 
 dotenv.config();
 
@@ -9,13 +10,15 @@ const PORT = process.env.PORT || 4242;
 const app = express();
 
 //middleware
-
 app.use(express.json());
+app.use(express.static("public"));
+
+// Serve index.html on root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
 app.use("/api/students", studentRouter);
-app.get("/", (req, res) => {
-  res.send("rest api with node.js and express");
-});
 
 app.use(errorHandler);
 
